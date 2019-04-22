cc4d965646366b7eb2dce3e2c83904336dbce074
Napoleon Jones <napoleon.jones@gotnext.io>
2019-04-21 22:46:12 -0400

    Revert "Updated Folders/Namespaces"
    This reverts commit 63a3f9fb315a79b5b2e8b74894214858936430b5.

--- OnlineToyStore.OrderProcessing/GetOrders.cs
+++ OnlineToyStore.OrderProcessing/GetOrders.cs
@@ -10,7 +10,7 @@ using Newtonsoft.Json;
 using OnlineToyStore.Core.Models;
 using System.Collections.Generic;
 
-namespace OnlineToyStore.Functions
+namespace OnlineToyStore.OrderProcessing
 {
     public static class GetOrders
     {
